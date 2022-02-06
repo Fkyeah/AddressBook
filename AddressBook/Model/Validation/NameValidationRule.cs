aaf70b9794ae5b19da29dcf5b0fe13cd4dbd74ba
Fkyeah <dimx63@mail.ru>
2022-02-06 15:55:46 +0300

    Перенос проекта в новый репозиторий

---
+++ AddressBook/Model/Validation/NameValidationRule.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace AddressBook.Model.Validation
+{
+    class NameValidationRule : ValidationRule
+    {
+        Regex regex = new Regex(@"^[A-Za-zА-Яа-я]{2,50}$");
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string login = value.ToString();
+            if (regex.IsMatch(login)) return new ValidationResult(true, null);
+            else return new ValidationResult(false, "Имя и фамилия могут состоять из букв с ограничением в 2-50 символов.\nПервый символ должен начинаться с буквы");
+        }
+    }
+}
